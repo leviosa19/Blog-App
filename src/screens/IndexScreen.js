8bdd62705a0e900aa2fa6106a37287e8336466ed
Rushi1618 <31608167+Rushi1618@users.noreply.github.com>
2020-12-28 19:07:10 +0530

    Upated

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,35 @@
+//import liraries
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext'
+
+// create a component
+const IndexScreen = () => {
+
+    const { data, addBlogPosts } = useContext(BlogContext)
+
+    return (
+        <View style={styles.container}>
+            <Text>IndexScreen</Text>
+            <Button title="Add Post" onPress={addBlogPosts} />
+            <FlatList
+                data={data}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) =>
+                    <Text>{item.title}</Text>
+                }
+            />
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
+
+//make this component available to the app
+export default IndexScreen;
